5687610d837d551e4b855e9edc852b88411f6363
student <student@ip-172-31-54-165.us-west-2.compute.internal>
2021-01-16 11:33:11 -0800

    good

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -1,9 +1,12 @@
 package _12_slot_machine;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -13,71 +16,147 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SlotMachine implements MouseListener {
+public class SlotMachine implements ActionListener{
 	static JFrame frame = new JFrame();	 
 	 static JPanel panel = new JPanel();
 	 static JButton button = new JButton();
-	private static Object imageURL;
-	private static Object limeURL;
-	private static Object cherryURL;
-	private static Object orangeURL;
+	 static JLabel label1 = new JLabel();
+	 static JLabel label2 = new JLabel();
+	 static JLabel label3 = new JLabel();
+	 static int randnum1 = 1;
+	 static int randnum2 = 2;
+	 static int randnum3 = 3;
 	public static void main(String[] args) {
 		frame.add(panel);
 		 panel.add(button);
 		 frame.setTitle("Slot Machine");
 		 frame.setVisible(true);
-		 frame.pack();
+		 SlotMachine machine = new SlotMachine();
+		 button.setText("Spin!");
+		 
+		 try {
+			label1 = machine.createLabelImage("lime.jpg");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 try {
+				label2 = machine.createLabelImage("cherry.jpg");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 try {
+				label3 = machine.createLabelImage("orange.jpg");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 	panel.add(label1);
+			panel.add(label2);
+			panel.add(label3);
+			machine.method();
+	}
+	public void method(){
+		button.addActionListener(this);
 	}
 	
-		private JLabel createLabelImage(String fileName) throws MalformedURLException{
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
         URL imageURL = getClass().getResource(fileName);
-        button.addMouseListener(this);
-        if (imageURL == null){
+	if (imageURL == null){
 		System.err.println("Could not find image " + fileName);
 		return new JLabel();
-        }
-       
+	}
+	Icon icon = new ImageIcon(imageURL);
+	JLabel imageLabel = new JLabel(icon);
+	return imageLabel;
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		Random ran = new Random();
+		randnum1 = ran.nextInt(3);
+		randnum2 = ran.nextInt(3);
+		randnum3 = ran.nextInt(3);
+		if(randnum1 == 0) {
+		try {
+			label1 = createLabelImage("orange.jpg");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			if(image.url == cherry.jpg) {
-			JOptionPane.showMessageDialog(null, "You got cherry!");
+		else if(randnum1 == 1) {
+			try {
+				label2 = createLabelImage("cherry.jpg");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			else if(image.url == orange.jpg) {
-				JOptionPane.showMessageDialog(null, "You got orange!");
+			}
+		else if(randnum1 == 2) {
+			try {
+				label3 = createLabelImage("lime.jpg");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+		if(randnum2 == 0) {
+			try {
+				label1 = createLabelImage("orange.jpg");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+			else if(randnum2 == 1) {
+				try {
+					label2 = createLabelImage("cherry.jpg");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			else {
-				JOptionPane.showMessageDialog(null, "You got lime! ");
+				}
+			else if(randnum2 == 2) {
+				try {
+					label3 = createLabelImage("lime.jpg");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				}
+		if(randnum3 == 0) {
+			try {
+				label1 = createLabelImage("orange.jpg");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		}
+			}
+			else if(randnum3 == 1) {
+				try {
+					label2 = createLabelImage("cherry.jpg");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				}
+			else if(randnum3 == 2) {
+				try {
+					label3 = createLabelImage("lime.jpg");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				}
+		frame.pack();
+	}
+}
+       
+		
 
 		
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		}
     
 	
 
