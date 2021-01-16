65ebab7a32a08ed93c11f45d53c3c735c947f4db
Jayh80961 <59904920+Jayh80961@users.noreply.github.com>
2021-01-16 10:35:14 -0800

    Add files via upload

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -1,5 +1,8 @@
 package _12_slot_machine;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.Icon;
@@ -7,32 +10,74 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements MouseListener {
 	static JFrame frame = new JFrame();	 
 	 static JPanel panel = new JPanel();
 	 static JButton button = new JButton();
 	private static Object imageURL;
-	public static JLabel main(String[] args) {
+	private static Object limeURL;
+	private static Object cherryURL;
+	private static Object orangeURL;
+	public static void main(String[] args) {
 		frame.add(panel);
 		 panel.add(button);
 		 frame.setTitle("Slot Machine");
 		 frame.setVisible(true);
 		 frame.pack();
-		 }
-		 JLabel createLabelImage(download.jpg); MalformedURLException{
-		        Object dowload;
-				URL imageURL = getClass().getResource(dowload.jpg);
-			if (imageURL == null){
-				System.err.println("Could not find image " + "dowload.jpg");
-				return new JLabel();
-			} 
-			Icon icon = new ImageIcon();
-			JLabel imageLabel = new JLabel(icon);
-			return imageLabel; 
-		
-
 	}
+	
+		private JLabel createLabelImage(String fileName) throws MalformedURLException{
+        URL imageURL = getClass().getResource(fileName);
+        button.addMouseListener(this);
+        if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+        }
+       
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			if(image.url == cherry.jpg) {
+			JOptionPane.showMessageDialog(null, "You got cherry!");
+			}
+			else if(image.url == orange.jpg) {
+				JOptionPane.showMessageDialog(null, "You got orange!");
+				}
+			else {
+				JOptionPane.showMessageDialog(null, "You got lime! ");
+			}
+		}
+
+		
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		}
+    
 	
 
