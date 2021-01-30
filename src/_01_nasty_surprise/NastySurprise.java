670a57f1fbc2a87a2b9f583fc526b12b9a7d01d1
student <student@ip-172-31-61-239.us-west-2.compute.internal>
2021-01-30 11:27:47 -0800

    gooood

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -1,5 +1,7 @@
 package _01_nasty_surprise;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -10,11 +12,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class NastySurprise {
+public class NastySurprise implements ActionListener {
 	private void showPictureFromTheInternet(String imageUrl) {
 	    try {
 	        URL url = new URL(imageUrl);
-	        Icon icon = new ImageIcon("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F430375308140588923%2F&psig=AOvVaw2_V9M4068YB4Rqkzh3YPUV&ust=1603647200593000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJir7L_hzewCFQAAAAAdAAAAABAD");
+	        Icon icon = new ImageIcon(url);
 	        JLabel imageLabel = new JLabel(icon);
 	        JFrame frame = new JFrame();
 	        frame.add(imageLabel);
@@ -28,7 +30,11 @@ public class NastySurprise {
 	static JButton button1 = new JButton();
 	static JLabel label = new JLabel();  
 	static JButton button2 = new JButton();
-	
+	static NastySurprise surprise = new NastySurprise();
+	public void add() {
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+	}
 	public static void main(String[] args) {
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
@@ -39,8 +45,20 @@ public class NastySurprise {
 	button2.setText("Treat");
 	panel.add(button1);
 	panel.add(button2);
+	surprise.add();
 	window.pack();
 	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(arg0.getSource().equals(button1)) {
+		surprise.showPictureFromTheInternet("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=640:*");
+		}
+		else {
+		surprise.showPictureFromTheInternet("https://d78ear04r5cyy.cloudfront.net/wp-content/uploads/bfi_thumb/halloween-2019-blog-1-1-347bsjou8sdvn8qy8efc5oeewxw9mtxd4hu4zm7adq07o7nge.jpg");
+		}
 	}
 	
 	
--- src/_11_whack_a_mole/whack_a_mole.java
+++ src/_11_whack_a_mole/whack_a_mole.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -13,15 +14,24 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class whack_a_mole implements ActionListener {
+	static int molecount = 0;
+	static int missed = 0;
 	static JFrame frame = new JFrame();	 
 	 static JPanel panel = new JPanel();
+	 static Date timeatstart;
 	 public static void main(String[] args) {
 		 Random random = new Random();
 		 whack_a_mole mole = new whack_a_mole();
 		 
 		 mole.make_buttons(random.nextInt(24));
-		 
+		 timeatstart = new Date();
 	 }
+	 private void endGame(Date timeAtStart, int molesWhacked) { 
+		    Date timeAtEnd = new Date();
+		    JOptionPane.showMessageDialog(null, "Your whack rate is "
+		            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+		                  + " moles per second.");
+		}
 	 public void make_buttons(int random){
 		for(int i=0;i<24;i++) {
 			JButton button = new JButton();
@@ -57,19 +67,25 @@ public class whack_a_mole implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		panel.removeAll();
 		JButton buttonPresssed = (JButton) arg0.getSource();
 		frame.dispose();
 		whack_a_mole mole = new whack_a_mole();
 		Random random = new Random();
 		mole.make_buttons(random.nextInt(24));
 		if((buttonPresssed.getText().equalsIgnoreCase("Mole!"))) {
-			
+			molecount++;
 		}
 		else {
 			JOptionPane.showMessageDialog(null, "You missed!");
+			missed++;
 		}
-		
+		if(molecount==10){
+			mole.endGame(timeatstart, molecount);
+		}
+		if(missed==5) {
+			JOptionPane.showMessageDialog(null, "You lose!");
+		}
 	}
 }
 	
