e22beb60079c9afeb24d794e3e581effb633a573
student <student@ip-172-31-63-202.us-west-2.compute.internal>
2021-02-20 11:16:33 -0800

    finished all

--- src/_09_latest_tweet/Latest_Tweet.java
+++ src/_09_latest_tweet/Latest_Tweet.java
@@ -1,17 +1,27 @@
 package _09_latest_tweet;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Latest_Tweet implements MouseListener {
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class Latest_Tweet implements ActionListener {
 	static JTextField text = new JTextField(20);
 	static JButton button = new JButton();
 	public static void main(String[] args) {
+		Latest_Tweet object = new Latest_Tweet();
 		JFrame window = new JFrame();
 		JPanel panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,35 +31,43 @@ public class Latest_Tweet implements MouseListener {
 		button.setText("Search the Twitterverse");
 		panel.add(text);
 		panel.add(button);
+		object.run();
 		window.pack();
 	
 	}
 	public void run() {
-		button.addMouseListener(this);
+		button.addActionListener(this);
+	}
+	private String getLatestTweet(String searchingFor) {
+
+	    Twitter twitter = new TwitterFactory().getInstance();
+
+	    AccessToken accessToken = new AccessToken(
+	        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+	        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+	    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+	        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+	    twitter.setOAuthAccessToken(accessToken);
+
+	Query query = new Query(searchingFor);
+	    try {
+	        QueryResult result = twitter.search(query);
+	        return result.getTweets().get(0).getText();
+	    } catch (Exception e) {
+	        System.err.print(e.getMessage());
+	        return "What the heck is that?";
+	    }
 	}
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		if(arg0.getSource()==button) {
+			JOptionPane.showMessageDialog(null, "Tweet,Tweet");
+			String tweetanswer = getLatestTweet("Hi");
+			JOptionPane.showMessageDialog(null, tweetanswer);
+		}
 		
 	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+
+	
 }
--- src/_10_pig_latin/piglatin.java
+++ src/_10_pig_latin/piglatin.java
@@ -1,33 +1,108 @@
 package _10_pig_latin;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class piglatin {
-	static JFrame frame = new JFrame();	 
-	 static JPanel panel = new JPanel();
-	 static JButton button = new JButton();
-	private static Object imageURL;
-	public static JLabel main(String[] args) {
-		frame.add(panel);
-		 panel.add(button);
-		 frame.setTitle("Slot Machine");
-		 frame.setVisible(true);
-		 frame.pack();
-		 }
-		 JLabel createLabelImage(download.jpg); MalformedURLException{
-		        Object dowload;
-				URL imageURL = getClass().getResource(dowload.jpg);
-			if (imageURL == null){
-				System.err.println("Could not find image " + "dowload.jpg");
-				return new JLabel();
-			} 
-			Icon icon = new ImageIcon();
-			JLabel imageLabel = new JLabel(icon);
-			return imageLabel; 
-		
+public class piglatin implements ActionListener {
+	static JButton button = new JButton();
+	static JTextField text1 = new JTextField(20);
+	 static JTextField text2 = new JTextField(20);
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();	 
+		 JPanel panel = new JPanel();
+		 frame.add(panel);
+		panel.add(text1);
+		panel.add(button);
+		panel.add(text2);
+		button.setText("Translate");
+		frame.setTitle("Jay's pig latin translater");
+		frame.setVisible(true);
+		frame.pack();
+		piglatin latin = new piglatin();
+		latin.run();
 
 	}
-}
+	public void run() {
+		button.addActionListener(this);
+	}
+
+		/**
+		* Method to test whether a character is a letter or not.
+		* @param c The character to test
+		* @return True if it's a letter
+		*/
+		private static boolean isLetter(char c) {
+		return ( (c >= 'A' && c <= 'Z') || (c >='a' && c <= 'z') );
+		}
+		    
+		/**
+		* Method to translate one word into pig latin.
+		* @param word The word in english
+		* @return The pig latin version
+		*/
+		private static String pigWord(String word) {
+		int split = firstVowel(word);
+		return word.substring(split)+"-"+word.substring(0, split)+"ay";
+		}
+			
+		/**
+		* Method to translate a sentence word by word.
+		* @param s The sentence in English
+		* @return The pig latin version
+		*/
+		public String translate(String s) {
+		String latin = "";
+		int i = 0;
+		while (i < s.length()) {
+		// Take care of punctuation and spaces
+		while (i < s.length() && !isLetter(s.charAt(i))) {
+		latin = latin + s.charAt(i);
+		i++;
+		}
+		// If there aren't any words left, stop.
+		if (i>=s.length()) break;
+		// Otherwise we're at the beginning of a word.
+		int begin = i;
+		while (i < s.length() && isLetter(s.charAt(i))) {
+		i++;
+		}
+		// Now we're at the end of a word, so translate it.
+		int end = i;
+		latin = latin + pigWord(s.substring(begin, end));
+		}
+		return latin;
+		}
+		/**
+		* Method to find the index of the first vowel in a word.
+		* @param word The word to search
+		* @return The index of the first vowel
+		*/
+			  private static int firstVowel(String word) {
+		word = word.toLowerCase();
+		for (int i=0; i < word.length(); i++)
+		if (word.charAt(i)=='a' || word.charAt(i)=='e' ||
+		word.charAt(i)=='i' || word.charAt(i)=='o' ||
+		word.charAt(i)=='u')
+		return i;
+		return 0;
+		}
+		@Override
+		
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			if(arg0.getSource()==button) {
+				String answer = translate(text1.getText());
+				text2.setText(answer);
+			}
+		}
+		}
+
+
+
+
